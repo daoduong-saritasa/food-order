2ac962c8195f6cbbfe42417ed2f728909ae2df5e
Dao Duong <dao.duong@saritasa.com>
2025-05-15 16:13:22 +0700

    Handle web view SVNW-99

--- src/app/(tabs)/query.tsx
+++ src/app/(tabs)/query.tsx
@@ -1,6 +1,13 @@
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import { type MockTodo } from "@/models/mockTodo";
 import { useTodos } from "@/store/queries/todo/todoQueries";
-import { ActivityIndicator, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Platform,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function QueryScreen() {
@@ -16,29 +23,43 @@ export default function QueryScreen() {
     );
   }
 
+  const todoList = (item: MockTodo) => (
+    <View key={item.id.toString()} className="flex-row py-2 gap-2">
+      <Text className="w-10">
+        {item.id}
+        .
+      </Text>
+      <Text className="flex flex-wrap flex-1">
+        {item.title}
+      </Text>
+      <View className="text-right text-black">
+        <IconSymbol
+          size={20}
+          name={item.completed ? "checkmark.circle.fill" : "info.circle.fill"}
+          color={item.completed ? "green" : "blue"}
+        />
+      </View>
+    </View>
+  );
+
   return (
     <SafeAreaView className="flex-1 items-center justify-center pb-16">
-      <View className="w-full px-4">
-        <ScrollView>
-          {data?.map(todo => (
-            <View key={todo.id} className="flex-row py-2 gap-2">
-              <Text className="w-10">
-                {todo.id}
-                .
-              </Text>
-              <Text className="flex flex-wrap flex-1">
-                {todo.title}
-              </Text>
-              <View className="text-right text-black">
-                <IconSymbol
-                  size={20}
-                  name={todo.completed ? "checkmark.circle.fill" : "info.circle.fill"}
-                  color={todo.completed ? "green" : "blue"}
-                />
+      <View className="w-full">
+        {Platform.OS === "web" ? (
+          <div className="overflow-y-auto max-h-[calc(100vh-48px)] px-4">
+            {data?.map(todoList)}
+          </div>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <View className="px-4">
+                {todoList(item)}
               </View>
-            </View>
-          ))}
-        </ScrollView>
+            )}
+            keyExtractor={item => item.id.toString()}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
