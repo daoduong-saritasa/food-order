9b22558312ae0cd9fc721138df656f7ac09c801d
Dao Duong <dao.duong@saritasa.com>
2025-05-15 17:06:01 +0700

    Improve example for socket page SVNW-99

--- src/app/(tabs)/socket.tsx
+++ src/app/(tabs)/socket.tsx
@@ -7,6 +7,8 @@ export default function SocketScreen() {
   const socket = useSocket();
   const [isConnected, setIsConnected] = useState(false);
   const [transport, setTransport] = useState("N/A");
+  const [name, setName] = useState("");
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     if (socket.connected) {
@@ -30,9 +32,16 @@ export default function SocketScreen() {
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
 
+    socket.on("count", setCount);
+
+    // Listen for 'name' messages from server
+    socket.on("name", setName);
+
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
+      socket.off("name", setName);
+      socket.off("count", setCount);
     };
   }, [socket]);
 
@@ -45,6 +54,14 @@ export default function SocketScreen() {
       <Text>
         Transport:
         {transport}
+      </Text>
+      <Text>
+        Name from server:
+        {name}
+      </Text>
+      <Text>
+        Count:
+        {count}
       </Text>
     </View>
   );
