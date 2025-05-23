712b6319bc07ffb553d7b76b6be9d5dd3e8d4f8f
quang-tien <tien.luu@saritasa.com>
2025-05-23 14:29:17 +0700

    Add edge spacing for devices SVNW-99

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,8 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "../../global.css";
 import QueryProviders from "./provider";
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -20,17 +22,24 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={DefaultTheme}>
       <QueryProviders>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
+        <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
               name="suppliers/[id]"
               options={{ headerShown: false }}
-          />
-          <Stack.Screen name="+not-found" options={{ title: "Not Found" }} />
-        </Stack>
+            />
+            <Stack.Screen name="+not-found" options={{ title: "Not Found" }} />
+          </Stack>
+        </SafeAreaView>
         <StatusBar style="auto" />
       </QueryProviders>
     </ThemeProvider>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
